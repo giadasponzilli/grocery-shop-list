02c121f919f021837306140331297e0310a4e08c
Giada Sponzilli <sponzilli.g@gmail.com>
2024-05-16 15:29:40 +0100

    added form and capitalization of first letter of each item added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,28 @@
 
-let textItem = document.getElementById("itemInput");
+let form = document.getElementById("form");
 
 let addItemButton = document.getElementById("addItemButton");
 
 
 function addItem() {
 
-  addItemButton.addEventListener("click", function(){
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
 
+    // Each time an item is added, a "li" is created with that input value and the text is capitalized
     let listItem = document.createElement("li")
-    listItem.innerHTML = itemInput.value.trim()
+    let itemValue = itemInput.value.trim()
+    listItem.innerHTML = itemValue.charAt(0).toUpperCase() + itemValue.slice(1)
 
+    // Add Checkbox
     let checkBox = document.createElement("input")
     checkBox.setAttribute("type", "checkbox")
     
+    // Add Remove Button
     let buttonRemove = document.createElement("button")
     buttonRemove.textContent = "Remove"
-    
+
+    // Append all created elements to html
     list.appendChild(listItem)
     listItem.appendChild(checkBox)
     listItem.appendChild(buttonRemove)
